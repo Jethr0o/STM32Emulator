653628a5984c4cc38b0d4f4e53ae2c3c4e7a0fbc
Jethr0o <110990229+Jethr0o@users.noreply.github.com>
2026-02-24 10:52:53 +0100

    new file:   RiverdiFirmware/Makefile 	new file:   RiverdiFirmware/mainFirm.c 	new file:   mainHw.repl 	new file:   startEmu.resc

---
+++ RiverdiFirmware/mainFirm.c
@@ -0,0 +1,21 @@
+// SDL libraries for rendering UI of Riverdi's display
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <stdlib.h>
+
+void imageLoop();
+
+int main(){
+	printf("Flavio Miccichè\n");
+	return 0;
+}
+
+
+void imageLoop(){
+
+	for(;;){
+
+	}
+
+}
