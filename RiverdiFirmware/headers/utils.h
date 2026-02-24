efd5672ed8b1f642ac555364932ec0c5c914b26a
Jethr0o <110990229+Jethr0o@users.noreply.github.com>
2026-02-24 16:29:01 +0100

    new file:   RiverdiFirmware/headers/utils.h

---
+++ RiverdiFirmware/headers/utils.h
@@ -0,0 +1,13 @@
+
+#define EXIT_FAILURE -1
+#define EXIT_SUCCESS  0
+#define RIVERDI_SCREEN_WIDTH 1024
+#define RIVERDI_SCREEN_HEIGHT 600
+
+static void imageLoop();
+static uint8_t _initAll_();
+
+// Error of the init
+enum commonErrors{
+
+};
